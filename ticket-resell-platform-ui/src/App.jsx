5393c8ee852ce38e5fcfe4efeb33036b1981f274
Arexmarx <phannguyenmanhcuong4662@gmail.com>
2024-09-14 10:27:42 +0700

    Add Buy Ticket Page

--- ticket-resell-platform-ui/src/App.jsx
+++ ticket-resell-platform-ui/src/App.jsx
@@ -5,16 +5,11 @@ import {
   HOME_PAGE,
   SIGN_UP_PAGE,
   LOGIN_PAGE,
-<<<<<<< HEAD
   EVENT_DETAIL_PAGE,
   BOUGHT_TICKET_MANEMENT_PAGE,
   PROFILE_PAGE,
   MY_SHOP_PAGE,
-=======
-  BOUGHT_TICKET_MANEMENT_PAGE,
-  PROFILE_PAGE,
-  MY_SHOP_PAGE,
-  EVENT_DETAIL_PAGE,
+  BUY_TICKET_PAGE,
 } from "./config/Constant";
 import HomePage from "./page/public/HomePage";
 import SignUp from "./page/public/SignUpPage";
@@ -22,11 +17,8 @@ import Login from "./page/public/LoginPage";
 import BoughtTicketManagementPage from './page/private/BoughtTicketManagementPage';
 import ProfilePage from "./page/private/ProfilePage";
 import MyShopPage from "./page/private/MyShopPage";
-
-
-
+import BuyTicketPage from "./page/private/BuyTicketPage"
 import EventDetail from "./page/public/EventDetailPage";
->>>>>>> 8105fcc46583f36d8b42bc8c927ce066fdce2582
 function App() {
   return (
     <div>
@@ -48,11 +40,9 @@ function App() {
 
           <Route path={MY_SHOP_PAGE} element={ <MyShopPage/> }></Route>
 
-<<<<<<< HEAD
           <Route path={EVENT_DETAIL_PAGE} element={ <EventDetail/> }></Route>
-
-=======
-          <Route path={EVENT_DETAIL_PAGE} element={<EventDetail />}></Route>
+          
+          <Route path={BUY_TICKET_PAGE} element={<BuyTicketPage/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
