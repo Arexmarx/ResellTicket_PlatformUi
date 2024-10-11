aac0d83529abf212cb4a8bd20589f5854693883a
Arexmarx <phannguyenmanhcuong4662@gmail.com>
2024-10-11 10:46:32 +0700

    Add search function

--- ticket-resell-platform-ui/src/components/SlideShowDetail.jsx
+++ ticket-resell-platform-ui/src/components/SlideShowDetail.jsx
@@ -1,16 +1,28 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
-import { MDBCarousel, MDBCarouselItem, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBIcon } from 'mdb-react-ui-kit';
-import { MDBTypography } from 'mdb-react-ui-kit';
+import { useEffect, useState } from "react";
+import {
+  MDBCarousel,
+  MDBCarouselItem,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import { MDBTypography } from "mdb-react-ui-kit";
 import { useNavigate } from "react-router-dom";
 import { EVENT_DETAIL_PAGE } from "../config/Constant";
-import { formatDateTime } from '../service/DateService.js';
+import { formatDateTime } from "../service/DateService.js";
 /**
  * Author: Phan Nguyễn Mạnh Cường
  */
 export default function SlideShowDetail({ Category, Events }) {
   // console.log("event: ",Events);
-  
+
   const items = Events;
   const [activeItem, setActiveItem] = useState(1);
   const itemsPerSlide = items.length > 4 ? 4 : items.length;
@@ -31,7 +43,6 @@ export default function SlideShowDetail({ Category, Events }) {
     for (let i = 0; i < itemsPerSlide; i++) {
       const index = (startIndex + i) % items.length;
       visibleItems.push(items[index]);
-      
     }
 
     return visibleItems;
@@ -40,37 +51,59 @@ export default function SlideShowDetail({ Category, Events }) {
   const navigate = useNavigate();
   const handleClick = (item) => {
     navigate(EVENT_DETAIL_PAGE, { state: { event: item } });
-  }
+  };
 
   //Auto-cycle
   useEffect(() => {
     const interval = setInterval(() => {
       handleNext();
-    },intervalTime)
+    }, intervalTime);
     return () => clearInterval(interval); // Clean up interval on unmount
-  },[]);
+  }, []);
 
-  return (
-    items ? 
+  return items ? (
     <MDBContainer className="my-5 position-relative">
-      <MDBTypography variant='h4' className='mb-4' color='black'>{Category}</MDBTypography>
+      <MDBTypography variant="h4" className="mb-4" color="black">
+        {Category}
+      </MDBTypography>
       <MDBCarousel showControls={false} showIndicators={false} fade>
         <MDBCarouselItem className="active">
           <MDBRow className="justify-content-center">
-            {
-              getVisibleItems().map((item) => (
-                <MDBCol style={{ cursor: 'pointer' }} key={item.id} md="3" className="mb-3">
-                  <MDBCard onClick={() => handleClick(item)}>
-                    <MDBCardImage style={{ maxHeight: '206px', minHeight: '206px' }} src={"data:image/png;base64, " + item.image} alt={item.name} position='top' />
-                    <MDBCardBody>
-                      <MDBCardTitle>{item.title}</MDBCardTitle>
-                      <MDBCardText>Ngày bắt đầu: {formatDateTime(item.startDate)}</MDBCardText> 
-                      <MDBCardText>Ngày kết thúc: {formatDateTime(item.endDate)}</MDBCardText>
-                    </MDBCardBody>
-                  </MDBCard>
-                </MDBCol>
-              ))
-            }
+            {getVisibleItems().map((item) => (
+              <MDBCol
+                style={{ cursor: "pointer" }}
+                key={item.id}
+                md="3"
+                className="mb-3"
+              >
+                <MDBCard onClick={() => handleClick(item)}>
+                  <MDBCardImage
+                    style={{ maxHeight: "206px", minHeight: "206px" }}
+                    src={"data:image/png;base64, " + item.image}
+                    alt={item.name}
+                    position="top"
+                  />
+                  <MDBCardBody>
+                    <MDBCardTitle
+                      title={item.name}
+                      style={{
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      {item.name}
+                    </MDBCardTitle>
+                    <MDBCardText>
+                      Ngày bắt đầu: {formatDateTime(item.startDate)}
+                    </MDBCardText>
+                    <MDBCardText>
+                      Ngày kết thúc: {formatDateTime(item.endDate)}
+                    </MDBCardText>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            ))}
           </MDBRow>
         </MDBCarouselItem>
       </MDBCarousel>
@@ -80,11 +113,11 @@ export default function SlideShowDetail({ Category, Events }) {
         size="2x"
         className="position-absolute top-50 translate-middle-y"
         style={{
-          cursor: 'pointer',
-          color: 'black',
-          left: '-40px', // Adjust this value to move the icon further left,
-          padding: '10px',
-          borderRadius: '50%',
+          cursor: "pointer",
+          color: "black",
+          left: "-40px", // Adjust this value to move the icon further left,
+          padding: "10px",
+          borderRadius: "50%",
         }}
         onClick={handlePrev}
       />
@@ -94,16 +127,16 @@ export default function SlideShowDetail({ Category, Events }) {
         size="2x"
         className="position-absolute top-50 translate-middle-y"
         style={{
-          cursor: 'pointer',
-          color: 'black',
-          right: '-40px', // Adjust this value to move the icon further right
-          padding: '10px',
-          borderRadius: '50%',
+          cursor: "pointer",
+          color: "black",
+          right: "-40px", // Adjust this value to move the icon further right
+          padding: "10px",
+          borderRadius: "50%",
         }}
         onClick={handleNext}
       />
     </MDBContainer>
-    : 
-    ''
+  ) : (
+    ""
   );
 }
--- ticket-resell-platform-ui/src/page/public/SearchPage.jsx
+++ ticket-resell-platform-ui/src/page/public/SearchPage.jsx
@@ -1,6 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
-import { ITEMS } from "../../test/DataTest";
 import {
   MDBCard,
   MDBCardBody,
@@ -17,41 +16,79 @@ import { useEffect, useState } from "react";
 import EventAPI from "../../service/api/EventAPI";
 import { formatDateTime } from "../../service/DateService";
 import { EVENT_DETAIL_PAGE } from "../../config/Constant";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function SearchPage() {
   const location = useLocation();
   const navigate = useNavigate();
   const { eventName } = location.state || {};
 
-  const [event, setEvent] = useState(null);
-  //   if (!event) {
-  //     return <div>No event data available</div>;
-  //   }
+  const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [date, setDate] =  useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      const eventResponse = await EventAPI.getHappeningEvents();
-      console.log(eventResponse.data.object);
-      setEvent(eventResponse.data.object);
+      try {
+        const eventResponse = await EventAPI.getHappeningEvents();
+        setEvents(eventResponse.data.object);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    fetchData().catch(console.log);
+    fetchData();
   }, []);
 
-  const handleClick  = (item) => {
-    navigate(EVENT_DETAIL_PAGE,{ state: { event: item } });
-  }
+  useEffect(() => {
+    // console.log("Component mounted or updated");
+    // console.log("Event name:", eventName);
+    // console.log("Events:", events);
+
+    if (events.length > 0) {
+      if (eventName) {
+        setFilteredEvents(
+          events.filter(
+            (eventItem) =>
+              eventItem.name &&
+              eventItem.name.toLowerCase().includes(eventName.toLowerCase())
+          )
+        );
+      } else {
+        setFilteredEvents(events);
+      }
+    } else {
+      console.log("No events to filter.");
+    }
+  }, [events, eventName]);
+
+  const handleClick = (item) => {
+    navigate(EVENT_DETAIL_PAGE, { state: { event: item } });
+  };
+
   return (
     <div>
       <Header />
       <MDBContainer className="my-5 position-relative">
         <MDBTypography style={{ paddingTop: "3%" }}>
-          Kết quả tìm kiếm của : {eventName}
+          Kết quả tìm kiếm của: {eventName}
         </MDBTypography>
-        <MDBRow style={{ marginTop: "5%" }}>
+        <MDBRow>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DatePicker"]}>
+              <DatePicker 
+              label="Chọn ngày bắt đầu sự kiện" 
+              onChange={(newValue) => setDate(newValue)}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
           
-          {event &&
-            event.length > 0 &&
-            event.map((eventItem, index) => (
+        </MDBRow>
+        <MDBRow style={{ marginTop: "3%" }}>
+          {filteredEvents.length > 0 ? (
+            filteredEvents.map((eventItem, index) => (
               <MDBCol key={index} md="3" style={{ marginTop: "4%" }}>
                 <MDBCard>
                   <MDBCardImage
@@ -61,8 +98,21 @@ export default function SearchPage() {
                     position="top"
                   />
                   <MDBCardBody>
-                    <MDBCardTitle>{eventItem.title}</MDBCardTitle>
-                    <MDBCardText>
+                    <MDBCardTitle
+                      title={eventItem.name}
+                      style={{
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      {eventItem.name}
+                    </MDBCardTitle>
+                    <MDBCardText
+                      style={{
+                        marginTop: eventItem.name.length > 22 ? "8%" : "8%",
+                      }}
+                    >
                       Ngày bắt đầu: {formatDateTime(eventItem.startDate)}
                     </MDBCardText>
                     <MDBCardText>
@@ -71,7 +121,10 @@ export default function SearchPage() {
                   </MDBCardBody>
                 </MDBCard>
               </MDBCol>
-            ))}
+            ))
+          ) : (
+            <div>No event data available</div>
+          )}
         </MDBRow>
       </MDBContainer>
       <Footer />
